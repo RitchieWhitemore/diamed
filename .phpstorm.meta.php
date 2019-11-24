b2076ba29e7ae925395b3dbf1d3236e0449719ac
RitchieWhitemore <RichiB@yandex.ru>
2019-11-24 14:53:58 +0300

    Установил kalnoy/nestedset

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
     /**
      * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-     * Generated on 2019-11-22 12:09:13.
+     * Generated on 2019-11-24 11:40:39.
      *
      * @author Barry vd. Heuvel <barryvdh@gmail.com>
      * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.5.2 on 2019-11-22 12:09:12.
+ * Generated for Laravel 6.5.2 on 2019-11-24 11:40:38.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
