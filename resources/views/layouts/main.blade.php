0ea003ec2e6cbb8f8d1754e511de3c7e6fd21b8c
RitchieWhitemore <RichiB@yandex.ru>
2019-11-27 15:45:38 +0300

    Подготовил проект к верстке

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<header>
+    <div class="logo__wrapper">
+        <img src="/img/logo.png">
+    </div>
+    <nav class="main-nav">
+        <ul class="main-nav__list">
+            <li class="main-nav__item"><a href="/" class="main-nav__link main-nav__link--active">Главная</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+</body>
+</html>
---
+++ resources/views/public/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+
+
+
+    <h1 class="title">Hello</h1>
+@endsection
