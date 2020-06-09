ae5e9a8c140b339e138ae176d1815a28012014aa
RitchieWhitemore <RichiB@yandex.ru>
2020-06-09 21:53:21 +0300

    Изменил соц иконки на шрифты

--- resources/views/public/part/_social.blade.php
+++ resources/views/public/part/_social.blade.php
@@ -1,6 +1,12 @@
-<li class="social__item"><a href="https://vk.com/diamed33" target="_blank" class="social__link social__link--vk">Вконтакте</a>
+<li class="social__item"><a href="https://vk.com/diamed33" target="_blank" class="social__link social__link--vk"><i
+            class="fab fa-vk"></i></a>
 </li>
 <li class="social__item"><a href="https://www.facebook.com/diamed33/" target="_blank"
-                            class="social__link social__link--fb">Facebook</a></li>
-<li class="social__item"><a href="https://ok.ru/diamed33" target="_blank" class="social__link social__link--ok">Одноклассники</a>
+                            class="social__link social__link--fb"><i class="fab fa-facebook-f"></i></a></li>
+<li class="social__item"><a href="https://instagram.com/iness_74" target="_blank"
+                            class="social__link social__link--inst"><i class="fab fa-instagram"></i></a>
 </li>
+<li class="social__item"><a href="https://ok.ru/diamed33" target="_blank" class="social__link social__link--ok"><i
+            class="fab fa-odnoklassniki"></i></a>
+</li>
+
